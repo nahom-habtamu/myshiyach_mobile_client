34f421df1781f9969d7a1a92f456ed51b00c8286
Nahom Habtamu <nahomhab2626@gmail.com>
2022-07-04 12:40:18 +0300

    Remove sliding animation in 1 image in post detail carousel

--- lib/presentation/widgets/post_detail/post_detail_carousel.dart
+++ lib/presentation/widgets/post_detail/post_detail_carousel.dart
@@ -1,7 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:mnale_client/presentation/pages/post_image_screen.dart';
 
+import '../../pages/post_image_screen.dart';
 import 'carousel_image_changer_icon.dart';
 import 'page_counter.dart';
 
@@ -24,41 +24,15 @@ class _PostDetailCarouselState extends State<PostDetailCarousel> {
     return Stack(
       alignment: Alignment.center,
       children: [
-        CarouselSlider(
-          carouselController: carouselController,
-          options: CarouselOptions(
-            viewportFraction: 1,
-            onPageChanged: (index, reason) {
-              setState(() {
-                currentCarouselIndex = index;
-              });
-            },
-          ),
-          items: widget.items
-              .map(
-                (item) => GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(
-                      context,
-                      PostImagesScreen.routeName,
-                      arguments: widget.items,
-                    );
-                  },
-                  child: Image.network(
-                    item,
-                    fit: BoxFit.cover,
-                    width: MediaQuery.of(context).size.width,
-                  ),
-                ),
-              )
-              .toList(),
-        ),
+        renderOneImageFallBack(),
+        renderCarouselSlider(),
         PageCounter(
           currentPage: currentCarouselIndex,
           pageCount: widget.items.length,
         ),
-        if (widget.items.length > 1)
-          CarouselImageChangerIcon(
+        Visibility(
+          visible: showCarousel,
+          child: CarouselImageChangerIcon(
             isRightArrow: true,
             onClick: () {
               carouselController.previousPage(
@@ -67,8 +41,10 @@ class _PostDetailCarouselState extends State<PostDetailCarousel> {
               );
             },
           ),
-        if (widget.items.length > 1)
-          CarouselImageChangerIcon(
+        ),
+        Visibility(
+          visible: showCarousel,
+          child: CarouselImageChangerIcon(
             onClick: () {
               carouselController.nextPage(
                 duration: const Duration(seconds: 1),
@@ -76,7 +52,65 @@ class _PostDetailCarouselState extends State<PostDetailCarousel> {
               );
             },
           ),
+        ),
       ],
+    );
+  }
+
+  bool get showCarousel => widget.items.length > 1;
+
+  renderOneImageFallBack() {
+    return Visibility(
+      visible: !showCarousel,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(
+            context,
+            PostImagesScreen.routeName,
+            arguments: widget.items,
+          );
+        },
+        child: Image.network(
+          widget.items.first,
+          fit: BoxFit.cover,
+          width: MediaQuery.of(context).size.width,
+        ),
+      ),
+    );
+  }
+
+  renderCarouselSlider() {
+    return Visibility(
+      visible: widget.items.length > 1,
+      child: CarouselSlider(
+        carouselController: carouselController,
+        options: CarouselOptions(
+          viewportFraction: 1,
+          onPageChanged: (index, reason) {
+            setState(() {
+              currentCarouselIndex = index;
+            });
+          },
+        ),
+        items: widget.items
+            .map(
+              (item) => GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(
+                    context,
+                    PostImagesScreen.routeName,
+                    arguments: widget.items,
+                  );
+                },
+                child: Image.network(
+                  item,
+                  fit: BoxFit.cover,
+                  width: MediaQuery.of(context).size.width,
+                ),
+              ),
+            )
+            .toList(),
+      ),
     );
   }
 }
