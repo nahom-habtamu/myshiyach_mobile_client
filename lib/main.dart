c1963f6a38ebdf13a9bc6e6de5517a42eb61f2ce
Nahom Habtamu <nahomhab2626@gmail.com>
2022-07-13 16:37:38 +0300

    Start FCM config

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,75 @@ import 'presentation/constants/app_level_state.dart';
 import 'presentation/constants/app_pages.dart';
 import 'presentation/pages/splash_page.dart';
 
+import 'package:firebase_messaging/firebase_messaging.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await di.init();
-  runApp(const MyApp());
+  runApp(const MessageHandler());
+}
+
+class MessageHandler extends StatefulWidget {
+  const MessageHandler({Key? key}) : super(key: key);
+
+  @override
+  State<MessageHandler> createState() => _MessageHandlerState();
+}
+
+class _MessageHandlerState extends State<MessageHandler> {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void testPushNotification() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    print('User granted permission: ${settings.authorizationStatus}');
+
+    final fcmToken = await FirebaseMessaging.instance.getToken();
+
+    print(fcmToken);
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    testPushNotification();
+    return MaterialApp(
+      title: 'My Shiyach',
+      theme: ThemeData(
+        primaryColor: const Color(0xff11435E),
+        fontFamily: 'DMSans',
+      ),
+      home: const SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Text('Hello'),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
