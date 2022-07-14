5dab9adfac440147b7b3de21f421f443490fd630
Nahom Habtamu <nahomhab2626@gmail.com>
2022-07-14 17:46:26 +0300

    Add Background message handler

--- lib/main.dart
+++ lib/main.dart
@@ -9,73 +9,51 @@ import 'presentation/pages/splash_page.dart';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+void testPushNotification(FirebaseMessaging messaging) async {
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+
+  print(fcmToken);
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print(
+          'Message also contained a notification: ${message.notification!.body}');
+    }
+  });
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await di.init();
-  runApp(const MessageHandler());
-}
-
-class MessageHandler extends StatefulWidget {
-  const MessageHandler({Key? key}) : super(key: key);
-
-  @override
-  State<MessageHandler> createState() => _MessageHandlerState();
-}
-
-class _MessageHandlerState extends State<MessageHandler> {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  testPushNotification(messaging);
 
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  void testPushNotification() async {
-    NotificationSettings settings = await messaging.requestPermission(
-      alert: true,
-      announcement: false,
-      badge: true,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
-      sound: true,
-    );
-
-    print('User granted permission: ${settings.authorizationStatus}');
-
-    final fcmToken = await FirebaseMessaging.instance.getToken();
-
-    print(fcmToken);
-
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Got a message whilst in the foreground!');
-      print('Message data: ${message.data}');
-
-      if (message.notification != null) {
-        print('Message also contained a notification: ${message.notification}');
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    testPushNotification();
-    return MaterialApp(
-      title: 'My Shiyach',
-      theme: ThemeData(
-        primaryColor: const Color(0xff11435E),
-        fontFamily: 'DMSans',
-      ),
-      home: const SafeArea(
-        child: Scaffold(
-          body: Center(
-            child: Text('Hello'),
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
