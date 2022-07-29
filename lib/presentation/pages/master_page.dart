79aba349d456633a7e1374d4cfef49fb70a23f84
Nahom Habtamu <nahomhab2626@gmail.com>
2022-07-29 17:47:28 +0300

    Add Message Overlay on MasterPage

--- lib/presentation/pages/master_page.dart
+++ lib/presentation/pages/master_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import '../../domain/enitites/conversation.dart';
 import '../bloc/auth/auth_cubit.dart';
 import '../bloc/auth/auth_state.dart';
 import '../bloc/get_all_conversations/get_all_conversations_cubit.dart';
@@ -51,70 +52,110 @@ class _MasterPageState extends State<MasterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: pagesToShow.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: const Icon(
-              Icons.pentagon_rounded,
-              size: 28,
+      bottomNavigationBar: SizedBox(
+        height: 70,
+        child: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: const Icon(
+                Icons.pentagon_rounded,
+                size: 28,
+              ),
+              label: AppLocalizations.of(context).masterNavigationBarTextOne,
             ),
-            label: AppLocalizations.of(context).masterNavigationBarTextOne,
+            renderChatItem(context),
+            BottomNavigationBarItem(
+              icon: const Icon(
+                Icons.all_inbox,
+                size: 28,
+              ),
+              label: AppLocalizations.of(context).masterNavigationBarTextThree,
+            ),
+            BottomNavigationBarItem(
+              icon: const Icon(
+                Icons.save,
+                size: 28,
+              ),
+              label: AppLocalizations.of(context).masterNavigationBarTextFour,
+            ),
+            BottomNavigationBarItem(
+              icon: const Icon(
+                Icons.settings,
+                size: 28,
+              ),
+              label: AppLocalizations.of(context).masterNavigationBarTextFive,
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: const Color(0xFF141313),
+          onTap: _onItemTapped,
+          type: BottomNavigationBarType.fixed,
+        ),
+      ),
+    );
+  }
+
+  BottomNavigationBarItem renderChatItem(BuildContext context) {
+    return BottomNavigationBarItem(
+      icon: Stack(
+        children: [
+          const Icon(
+            Icons.chat,
+            size: 25,
           ),
-          BottomNavigationBarItem(
-            icon: Stack(
-              children: const [
-                Icon(
-                  Icons.chat,
-                  size: 25,
-                ),
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  child: CircleAvatar(
-                    backgroundColor: Colors.red,
-                    radius: 7,
-                    child: Center(
-                      child: Text(
-                        "2",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 10,
+          BlocBuilder<GetAllConversationsCubit, Stream<List<Conversation>>>(
+            builder: (context, conversationStream) {
+              return StreamBuilder<List<Conversation>>(
+                stream: conversationStream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError ||
+                      snapshot.data == null ||
+                      snapshot.data!.isEmpty) {
+                    return Container();
+                  } else if (snapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return Container();
+                  }
+
+                  int unreadMessagesCount = buildUnreadMessages(snapshot.data);
+                  return Visibility(
+                    visible: unreadMessagesCount > 0,
+                    child: Positioned(
+                      top: 2,
+                      left: 5,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.red,
+                        radius: 6.5,
+                        child: Center(
+                          child: Text(
+                            unreadMessagesCount.toString(),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 11,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-              ],
-            ),
-            label: AppLocalizations.of(context).masterNavigationBarTextTwo,
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(
-              Icons.all_inbox,
-              size: 28,
-            ),
-            label: AppLocalizations.of(context).masterNavigationBarTextThree,
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(
-              Icons.save,
-              size: 28,
-            ),
-            label: AppLocalizations.of(context).masterNavigationBarTextFour,
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(
-              Icons.settings,
-              size: 28,
-            ),
-            label: AppLocalizations.of(context).masterNavigationBarTextFive,
+                  );
+                },
+              );
+            },
           ),
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: const Color(0xFF141313),
-        onTap: _onItemTapped,
-        type: BottomNavigationBarType.fixed,
       ),
+      label: AppLocalizations.of(context).masterNavigationBarTextTwo,
     );
+  }
+
+  int buildUnreadMessages(List<Conversation>? data) {
+    var totalUnreadMessagesCount = 0;
+    for (var conversation in data!) {
+      var unreadMessagesInConversation =
+          conversation.messages.where((m) => !m.isSeen).toList();
+      totalUnreadMessagesCount += unreadMessagesInConversation.length;
+    }
+
+    return totalUnreadMessagesCount;
   }
 }
