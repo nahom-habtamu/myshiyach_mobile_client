fb211498416325650792e07e4e423e5ad24261b8
Nahom Habtamu <nahomhab2626@gmail.com>
2022-10-08 11:07:11 +0300

    add searching ability to look from all products instead of currently loaded products

--- lib/presentation/widgets/home/product_grid_with_pagination.dart
+++ lib/presentation/widgets/home/product_grid_with_pagination.dart
@@ -18,13 +18,122 @@ import '../common/error_content.dart';
 import 'custom_footer_for_lazy_loading.dart';
 import 'product_grid_item.dart';
 
-class ProductList extends StatefulWidget {
+class ProductGridWithOutPagination extends StatefulWidget {
+  List<Product> favorites;
+  final List<Product> products;
+  final String searchKeyword;
+  ProductGridWithOutPagination({
+    Key? key,
+    required this.products,
+    required this.favorites,
+    required this.searchKeyword,
+  }) : super(key: key);
+
+  @override
+  State<ProductGridWithOutPagination> createState() =>
+      _ProductGridWithOutPaginationState();
+}
+
+class _ProductGridWithOutPaginationState
+    extends State<ProductGridWithOutPagination> {
+  SetFavoriteProductsCubit? setFavoriteProductsCubit;
+  final RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget buildErrorContent() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.5,
+        child: ErrorContent(
+          onButtonClicked: () => {},
+        ),
+      ),
+    );
+  }
+
+  Widget buildEmptyStateContent() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.5,
+        child: FallBackContent(
+          captionText: AppLocalizations.of(context).homeEmptyProductText,
+          hintText:
+              AppLocalizations.of(context).homeRetryFetchProductButtonLabel,
+          buttonText:
+              AppLocalizations.of(context).homeRetryFetchProductButtonText,
+          onButtonClicked: () {},
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: true,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 5.0,
+        mainAxisSpacing: 5.0,
+        childAspectRatio: MediaQuery.of(context).size.width /
+            (MediaQuery.of(context).size.height) *
+            1.47,
+      ),
+      itemCount: widget.products.length,
+      itemBuilder: (context, index) {
+        return buildProduct(widget.products[index]);
+      },
+    );
+  }
+
+  ProductGridItem buildProduct(Product product) {
+    var duplicate =
+        widget.favorites.where((element) => element.id == product.id).toList();
+    return ProductGridItem(
+      product: product,
+      isFavorite: duplicate.isEmpty,
+      onFavoritesTap: () {
+        updateFavorites(duplicate, product);
+      },
+    );
+  }
+
+  void updateFavorites(List<Product> duplicate, Product product) {
+    if (duplicate.isNotEmpty) {
+      widget.favorites.removeWhere((element) => element.id == product.id);
+      setState(() {});
+    } else {
+      setState(() {
+        widget.favorites = [...widget.favorites, product];
+      });
+    }
+    List<ProductModel> favoritesToSave = parseListToProductModelList();
+    context
+        .read<SetFavoriteProductsCubit>()
+        .setFavoriteProducts
+        .call(favoritesToSave);
+  }
+
+  List<ProductModel> parseListToProductModelList() {
+    var favoritesToSave =
+        widget.favorites.map((e) => ProductModel.fromProduct(e)).toList();
+    return favoritesToSave;
+  }
+}
+
+////////////////////////////
+
+class ProductGridWithPagination extends StatefulWidget {
   List<Product> favorites;
   final List<Product> products;
   final PageAndLimitModel? pageAndLimit;
   final FilterCriteriaModel? filterValues;
   final Function(Loaded, List<Product>) onRefreshed;
-  ProductList({
+  ProductGridWithPagination({
     Key? key,
     required this.products,
     required this.favorites,
@@ -34,10 +143,11 @@ class ProductList extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<ProductList> createState() => _ProductListState();
+  State<ProductGridWithPagination> createState() =>
+      _ProductGridWithPaginationState();
 }
 
-class _ProductListState extends State<ProductList> {
+class _ProductGridWithPaginationState extends State<ProductGridWithPagination> {
   SetFavoriteProductsCubit? setFavoriteProductsCubit;
   final RefreshController _refreshController =
       RefreshController(initialRefresh: false);
