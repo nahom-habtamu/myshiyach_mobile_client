7968452b9fae6e9c2ee90ada8bcb9c55d77a3092
Nahom Habtamu <nahomhab2626@gmail.com>
2023-01-28 13:04:26 +0300

    change getrecommended items to read from paginated instead of all products

--- lib/domain/usecases/get_products_by_category.dart
+++ lib/domain/usecases/get_products_by_category.dart
@@ -1,3 +1,9 @@
+import 'package:mnale_client/data/models/filter/filter_criteria_model.dart';
+import 'package:mnale_client/data/models/product/page_and_limit_model.dart';
+import 'package:mnale_client/domain/enitites/main_category.dart';
+import 'package:mnale_client/domain/enitites/page_and_limit.dart';
+import 'package:mnale_client/domain/enitites/sub_category.dart';
+
 import '../../core/utils/date_time_formatter.dart';
 import '../contracts/product_service.dart';
 import '../enitites/product.dart';
@@ -8,9 +14,26 @@ class GetProductsByCategory {
   GetProductsByCategory(this.repository);
 
   Future<List<Product>> call(String mainCategory, String subCategory) async {
-    var parsedResult = await repository.getAllProducts();
-    parsedResult.sort(((a, b) => _compareRefreshedAt(a, b)));
-    return parsedResult
+    var parsedResult = await repository.getPaginatedProducts(
+      PageAndLimitModel(limit: 60, page: 1),
+      FilterCriteriaModel(
+        maxPrice: 0,
+        minPrice: 0,
+        mainCategory:
+            MainCategory(id: mainCategory, title: "", subCategories: []),
+        subCategory: SubCategory(id: subCategory, title: ""),
+        brand: null,
+        city: null,
+        sortByPriceAscending: null,
+        sortByCreatedByAscending: null,
+        keyword: null,
+      ),
+    );
+
+    var products = parsedResult.products;
+
+    products.sort(((a, b) => _compareRefreshedAt(a, b)));
+    return products
         .where((element) =>
             element.mainCategory == mainCategory &&
             element.subCategory == subCategory)
