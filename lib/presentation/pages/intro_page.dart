ea0cdf6fe56e91feb91d21ed9bdeaddb1538feab
Nahom Habtamu <nahomhab2626@gmail.com>
2023-02-04 11:40:00 +0300

    add swipe functionality on intro page

--- lib/presentation/pages/intro_page.dart
+++ lib/presentation/pages/intro_page.dart
@@ -16,6 +16,8 @@ class _IntroPageState extends State<IntroPage> {
   int currentInformationIndex = 0;
   dynamic introData = [];
 
+  final PageController _controller = PageController();
+
   @override
   void initState() {
     super.initState();
@@ -50,96 +52,127 @@ class _IntroPageState extends State<IntroPage> {
       child: Scaffold(
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 25,
-                ),
-                renderSkipButton(),
-                const SizedBox(
-                  height: 25,
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: 301,
-                  child: Image.asset(
-                    introData.elementAt(currentInformationIndex)["image"]!,
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 25,
                   ),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  introData.elementAt(currentInformationIndex)["header"]!,
-                  style: const TextStyle(
-                    color: Color(0xff1B1D21),
-                    fontSize: 36,
-                    fontWeight: FontWeight.w700,
+                  renderSkipButton(),
+                  const SizedBox(
+                    height: 25,
                   ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  introData.elementAt(currentInformationIndex)["description"]!,
-                  style: const TextStyle(
-                    height: 1.5,
-                    color: Colors.grey,
-                    fontSize: 16,
-                    letterSpacing: 0.36,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                renderProgressIndicatorBubbles(),
-                const SizedBox(
-                  height: 25,
-                ),
-                SizedBox(
-                  height: 50,
-                  width: MediaQuery.of(context).size.width,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (currentInformationIndex < introData.length - 1) {
+                  SizedBox(
+                    height: 600,
+                    child: PageView(
+                      controller: _controller,
+                      onPageChanged: (value) {
                         setState(() {
-                          currentInformationIndex++;
+                          currentInformationIndex = value;
                         });
-                      } else {
-                        Navigator.pushReplacementNamed(
-                          context,
-                          LoginPage.routeName,
-                        );
-                      }
-                    },
-                    child: Text(
-                      AppLocalizations.of(context).introPageNextButtonText,
+                      },
+                      children: [
+                        renderMainContent(),
+                        renderMainContent(),
+                        renderMainContent(),
+                      ],
                     ),
-                    style: ElevatedButton.styleFrom(
-                      primary: const Color(0xff11435E),
-                      textStyle: const TextStyle(
-                        color: Colors.white,
+                  ),
+                  renderProgressIndicatorBubbles(),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (currentInformationIndex < introData.length - 1) {
+                          setState(() {
+                            currentInformationIndex++;
+                          });
+                          _controller.animateToPage(
+                            currentInformationIndex,
+                            duration: const Duration(milliseconds: 200),
+                            curve: Curves.bounceInOut,
+                          );
+                        } else {
+                          Navigator.pushReplacementNamed(
+                            context,
+                            LoginPage.routeName,
+                          );
+                        }
+                      },
+                      child: Text(
+                        AppLocalizations.of(context).introPageNextButtonText,
                       ),
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(16),
+                      style: ElevatedButton.styleFrom(
+                        primary: const Color(0xff11435E),
+                        textStyle: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(16),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-              ],
+                  const SizedBox(
+                    height: 30,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Column renderMainContent() {
+    return Column(
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: 301,
+          child: Image.asset(
+            introData.elementAt(currentInformationIndex)["image"]!,
+          ),
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        Text(
+          introData.elementAt(currentInformationIndex)["header"]!,
+          style: const TextStyle(
+            color: Color(0xff1B1D21),
+            fontSize: 36,
+            fontWeight: FontWeight.w700,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          introData.elementAt(currentInformationIndex)["description"]!,
+          style: const TextStyle(
+            height: 1.5,
+            color: Colors.grey,
+            fontSize: 16,
+            letterSpacing: 0.36,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+      ],
     );
   }
 
@@ -195,5 +228,3 @@ class _IntroPageState extends State<IntroPage> {
     );
   }
 }
-
-
